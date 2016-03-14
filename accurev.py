30f8162a2df58177b9d79efd047181b6489b6483
Akash Amat <Vyoam@users.noreply.github.com>
2016-03-14 14:20:42 +0530

    Update accurev.py
    Tabs to spaces

--- accurev.py
+++ accurev.py
@@ -23,76 +23,76 @@ commandField2=None
 resultListView=None
 
 def main():
-	global window
-	global commandField1
-	global commandField2
-	global resultListView
-	
-	window=Tkinter.Tk()
-	window.title('AccuRev Lightweight Helper')
-	window.lift()
-	window.attributes('-topmost', True)
-	window.attributes('-topmost', False)
-	
-	commandField1=Tkinter.Entry(window)
-	commandField1.bind('<Return>', inputEntry1Action)
-	commandField1.pack()
-	
-	#Previosuly had tried the basic list - non-tree, w/o columns
-	#resultListView=Tkinter.Listbox(window, selectmode=Tkinter.EXTENDED)
-	#resultListView.pack()
-	
-	resultListView=MultiColumnListbox()
-	
-	commandField2=Tkinter.Entry(window)
-	#commandField2.bind('<Return>', button2Action)
-	#disabled Enter key binding to avoid rash commands for the final step
-	commandField2.pack()
-	
-	button2=Tkinter.Button(window, text="Confirm and Execute", command=button2Action)
-	button2.pack()
-	
-	Tkinter.mainloop()
+    global window
+    global commandField1
+    global commandField2
+    global resultListView
+    
+    window=Tkinter.Tk()
+    window.title('AccuRev Lightweight Helper')
+    window.lift()
+    window.attributes('-topmost', True)
+    window.attributes('-topmost', False)
+    
+    commandField1=Tkinter.Entry(window)
+    commandField1.bind('<Return>', inputEntry1Action)
+    commandField1.pack()
+    
+    #Previosuly had tried the basic list - non-tree, w/o columns
+    #resultListView=Tkinter.Listbox(window, selectmode=Tkinter.EXTENDED)
+    #resultListView.pack()
+    
+    resultListView=MultiColumnListbox()
+    
+    commandField2=Tkinter.Entry(window)
+    #commandField2.bind('<Return>', button2Action)
+    #disabled Enter key binding to avoid rash commands for the final step
+    commandField2.pack()
+    
+    button2=Tkinter.Button(window, text="Confirm and Execute", command=button2Action)
+    button2.pack()
+    
+    Tkinter.mainloop()
 
 def inputEntry1Action(event=None):
-	global commandField1
-	#Keeping these arrow as comment as they might be useful as other kind of separator
-	#Here, the 'in and out' representations seem better
-	#print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-	print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-	print 'Command 1 Started...'
-	#print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-	print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-	command1=commandField1.get()
-	###Maybe check and just use .call() for commands not requiring parsing?
-	commandOutputStr=subprocess.check_output(command1)
-	print commandOutputStr
-	#print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-	print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-	print 'Command 1 Complete'
-	###Remove below call if using .call()
-	if(re.search(r'<AcResponse',commandOutputStr)!=None):
-		parseAndFillList(commandOutputStr)
-	#print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-	print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<';
+    global commandField1
+    #Keeping these arrow as comment as they might be useful as other kind of separator
+    #Here, the 'in and out' representations seem better
+    #print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    print 'Command 1 Started...'
+    #print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    command1=commandField1.get()
+    ###Maybe check and just use .call() for commands not requiring parsing?
+    commandOutputStr=subprocess.check_output(command1)
+    print commandOutputStr
+    #print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    print 'Command 1 Complete'
+    ###Remove below call if using .call()
+    if(re.search(r'<AcResponse',commandOutputStr)!=None):
+        parseAndFillList(commandOutputStr)
+    #print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<';
 
 def button2Action(event=None):
-	global commandField2
-	#Keeping these arrow as comment as they might be useful as other kind of separator
-	#Here, the 'in and out' representations seem better
-	#print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-	print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-	print 'Command 2 Started...'
-	#print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-	print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-	command2=commandField2.get()
-	getSelectedAndCall(command2)
-	#print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-	print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-	print 'Command 2 Complete'
-	#print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-	print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-	
+    global commandField2
+    #Keeping these arrow as comment as they might be useful as other kind of separator
+    #Here, the 'in and out' representations seem better
+    #print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    print 'Command 2 Started...'
+    #print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    command2=commandField2.get()
+    getSelectedAndCall(command2)
+    #print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    print 'Command 2 Complete'
+    #print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    
 # the test data ...
 my_headers = ['column-1', 'column-2']
 my_list = [
@@ -105,49 +105,50 @@ my_list = [
 ###todo
 #pass the headings as well as the items to populate the new list
 def parseAndFillList(dataListStr):
-	global resultListView
-	resultListView.tree.delete(*(resultListView.tree.get_children()))
-	print 'Parsing Result List...'
-	
-	# using split for standard list (non-xml)
-	#list=dataListStr.split('\r\n')
-	#for i, row in enumerate(list):
-	#	list[i]=((row.split(' '))[0],'Data')
-	
-	list=[]
-	timeFormat="%d %B %Y %H:%M:%S"  #%c : The preferred date and time representation for the current locale.
-	
-	treeRoot=ET.fromstring(dataListStr)
-	for child in treeRoot:
-		if child.tag == 'element':
-			filePath=child.attrib['location']
-			time=datetime.fromtimestamp( float(child.attrib['modTime']) ).strftime(timeFormat)
-			list.append( (filePath, time) )
-	
-	print list
-	resultListView._build_tree(items=list)
-	print 'Parsing Done'
+    global resultListView
+    resultListView.tree.delete(*(resultListView.tree.get_children()))
+    print 'Parsing Result List...'
+    
+    # using split for standard list (non-xml)
+    #list=dataListStr.split('\r\n')
+    #for i, row in enumerate(list):
+    #   list[i]=((row.split(' '))[0],'Data')
+    
+    list=[]
+    timeFormat="%d %B %Y %H:%M:%S"  #%c : The preferred date and time representation for the current locale.
+    
+    treeRoot=ET.fromstring(dataListStr)
+    for child in treeRoot:
+        if child.tag == 'element':
+            filePath=child.attrib['location']
+            time=datetime.fromtimestamp( float(child.attrib['modTime']) ).strftime(timeFormat)
+            list.append( (filePath, time) )
+    
+    print list
+    resultListView._build_tree(items=list)
+    print 'Parsing Done'
 
 def getSelectedAndCall(commandStr):
-	global resultListView
-	print 'Appending Selected Items to the Command...'
-	appendStr=commandStr
-	for resultItem in resultListView.tree.selection():
-		#resultItem is a coded identifier for the whole row
-		temp = resultListView.tree.set(resultItem)
-		print temp[temp.keys()[0]]
-		appendStr=appendStr+' '+temp[temp.keys()[0]]
-	print appendStr
-	subprocess.call(appendStr)
-	print 'Executing Command...'
-	
+    global resultListView
+    print 'Appending Selected Items to the Command...'
+    appendStr=commandStr
+    for resultItem in resultListView.tree.selection():
+        #resultItem is a coded identifier for the whole row
+        temp = resultListView.tree.set(resultItem)
+        print temp[temp.keys()[0]]
+        appendStr=appendStr+' '+temp[temp.keys()[0]]
+    print appendStr
+    subprocess.call(appendStr)
+    print 'Executing Command...'
+    
+###Add reference source
 """use a ttk.TreeView as a multicolumn ListBox"""
 class MultiColumnListbox(object):
 
     def __init__(self):
         self.tree = None
         self._setup_widgets()
-		#disabled loading of test data
+        #disabled loading of test data
         #self._build_tree()
 
     def _setup_widgets(self):
@@ -189,8 +190,8 @@ class MultiColumnListbox(object):
                 col_w = tkFont.Font().measure(val)
                 if self.tree.column(titles[ix],width=None)<col_w:
                     self.tree.column(titles[ix], width=col_w)
-		
-		sortby(self.tree, titles[1], 1)
+        
+        sortby(self.tree, titles[1], 1)
 
 def sortby(tree, col, descending):
     """sort tree contents when a column header is clicked on"""
@@ -206,5 +207,5 @@ def sortby(tree, col, descending):
     # switch the heading so it will sort in the opposite direction
     tree.heading(col, command=lambda col=col: sortby(tree, col, \
         int(not descending)))
-		
+        
 main()
