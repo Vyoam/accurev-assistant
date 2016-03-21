82c9962519bc07043246f04239abe2267633c7b4
aamat <aamat@visa.com>
2016-03-21 14:36:04 +0530

    modifs: command list widget alignments wildcard style key matching date sorting correction by prefixing orig float value add traceback.print_exc call

--- accurev.py
+++ accurev.py
@@ -6,6 +6,8 @@ This was created out of the need to mitigate the slowness of AccuRev GUI ...
 ... by leveraging the faster and detailed CLI responses.
 '''
 
+# to do? http://stackoverflow.com/questions/6548837/how-do-i-get-an-event-callback-when-a-tkinter-entry-widget-is-modified link to activate custom when field changed manually?
+
 import Tkinter
 import subprocess
 
@@ -18,6 +20,9 @@ import xml.etree.ElementTree as ET
 import socket
 import os
 
+import sys #
+import traceback #
+
 from datetime import datetime
 
 window=None
@@ -25,11 +30,36 @@ commandField1=None
 commandField2=None
 resultListView=None
 
+defaultCommandOptionStr="List modified elements"
+defaultCommandOptionValStr='accurev stat -f x -m'
+
+commandOptionsDict = {defaultCommandOptionStr:defaultCommandOptionValStr,
+                      "List kept":'accurev stat -f x -k',
+                      "List pending":'accurev stat -f x -p',
+                      "List overlapping":'accurev stat -f x -o',
+                      "List external":'accurev stat -f x -x',
+                      "List missing":'accurev stat -f x -M',
+                      "List default group":'accurev stat -f x -d',
+                      "Update preview":'accurev update -i -f x', #response doesn't return status... maybe one of the reason GUI is slow... it fetches status one by one?
+                      "Custom command":'accurev '}
+
+defaultCommandOptionStr2="Keep selected"
+defaultCommandOptionValStr2='accurev keep -c ""'
+
+commandOptionsDict2 = {defaultCommandOptionStr2:defaultCommandOptionValStr2,
+                      "Revert to backed":'accurev purge',
+                      "Promote selected":'accurev promote -c ""',
+                      "Custom command":'accurev '}                   
+                      
 def main():
     global window
     global commandField1
     global commandField2
     global resultListView
+    global defaultCommandOptionStr
+    global commandOptionsDict
+    global defaultCommandOptionStr2
+    global commandOptionsDict2
     
     #using accurev, detect the workspace folder for the system and cd to that folder
     changeToWspaceDir()
@@ -40,20 +70,57 @@ def main():
     window.attributes('-topmost', True)
     window.attributes('-topmost', False)
     
-    commandField1=Tkinter.Entry(window)
-    commandField1.bind('<Return>', inputEntry1Action)
-    commandField1.pack()
+    label1Text = 'Select a command from list, or type in the field...\nThen press enter or the button to enlist elements...'
+    label1 = ttk.Label(window, wraplength="4i", justify="left", anchor="n", padding=(10, 2, 10, 6), text=label1Text)#, background="DarkSeaGreen2" , font="default 9 bold")
+    label1.pack(fill='x')
     
+    #>>>
+    container1=ttk.Frame(window)
+    container1.pack()
+    
+    defaultCommandOption=Tkinter.StringVar(container1)
+    defaultCommandOption.set(defaultCommandOptionStr)
+    commandOptions=commandOptionsDict.keys()
+    commandMenu=Tkinter.OptionMenu(container1, defaultCommandOption, *commandOptions, command=fillCommandField)
+    commandMenu.config(bg="DarkSeaGreen2")
+    commandMenu.pack(side=Tkinter.LEFT, padx=10)
+    
+    commandField1=Tkinter.Entry(container1, bg="DarkSeaGreen1")
+    commandField1.bind('<Return>', inputEntry1Action)
+    commandField1.pack(side=Tkinter.LEFT, padx=10)
+    
+    button1=Tkinter.Button(container1, text="Execute", command=inputEntry1Action, bg="SeaGreen3")
+    button1.pack(side=Tkinter.LEFT, padx=10)
+    #<<<
+        
     resultListView=MultiColumnListbox()
     
-    commandField2=Tkinter.Entry(window)
+    label2Text = 'Select elements and apply operation...\nWhere required, insert comments inside the quotes...'
+    label2 = ttk.Label(window, wraplength="4i", justify="left", anchor="n", padding=(10, 2, 10, 6), text=label2Text)
+    label2.pack(fill='x')
+    
+    #>>>
+    container2=ttk.Frame(window)
+    container2.pack()
+    
+    defaultCommandOption2=Tkinter.StringVar(container2)
+    defaultCommandOption2.set(defaultCommandOptionStr2)
+    commandOptions2=commandOptionsDict2.keys()
+    commandMenu2=Tkinter.OptionMenu(container2, defaultCommandOption2, *commandOptions2, command=fillCommandField2)
+    commandMenu2.config(bg="DarkSlateGray2")
+    commandMenu2.pack(side=Tkinter.LEFT, padx=10)
+    
+    commandField2=Tkinter.Entry(container2, bg="DarkSlateGray1")
     #commandField2.bind('<Return>', button2Action)
     #disabled Enter key binding to avoid rash commands for the final step
-    commandField2.pack()
+    commandField2.pack(side=Tkinter.LEFT, padx=10)
     
-    button2=Tkinter.Button(window, text="Confirm and Execute", command=button2Action)
-    button2.pack()
+    button2=Tkinter.Button(container2, text="Confirm and Execute", command=button2Action, bg="RoyalBlue1")
+    button2.pack(side=Tkinter.LEFT, padx=10)
+    #<<<
     
+    fillCommandField(defaultCommandOptionStr)
+    fillCommandField2(defaultCommandOptionStr2)
     Tkinter.mainloop()
 
 def changeToWspaceDir():    
@@ -65,7 +132,8 @@ def changeToWspaceDir():
     except Exception as e:
         print 'Error...'
         print e.output
-        if(re.search(r'Not authenticated', e.output)!=None):
+        if(re.search(r'Not authenticated', e.output)!=None) or (re.search(r'Expired', e.output)!=None):
+            print 'Login...'
             subprocess.call('accurev login')
             wspaceResult=subprocess.check_output(getWspaceCmd)
         else:
@@ -100,7 +168,7 @@ def inputEntry1Action(event=None):
     print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
     print 'Command 1 Complete'
     ###Remove below call if using .call()
-    if(re.search(r'<AcResponse',commandOutputStr)!=None):
+    if(re.search(r'<AcResponse',commandOutputStr, re.IGNORECASE)!=None):
         parseAndFillList(commandOutputStr)
     print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<';
 
@@ -115,6 +183,20 @@ def button2Action(event=None):
     print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
     print 'Command 2 Complete'
     print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    
+def fillCommandField(option):
+    global commandField1
+    global commandOptionsDict
+    
+    commandField1.delete(0,Tkinter.END)
+    commandField1.insert(0,commandOptionsDict[option])
+    
+def fillCommandField2(option):
+    global commandField2
+    global commandOptionsDict2
+    
+    commandField2.delete(0,Tkinter.END)
+    commandField2.insert(0,commandOptionsDict2[option])
     
 # the test data ...
 my_headers = ['location', 'modTime', 'status', 'namedVersion']
@@ -140,10 +222,25 @@ def parseAndFillList(dataListStr):
         treeRoot=ET.fromstring(dataListStr)
         for child in treeRoot:
             if child.tag.lower() == 'element':
-                filePath=child.attrib['location']
-                time=datetime.fromtimestamp( float(child.attrib['modTime']) ).strftime(timeFormat)
-                status=child.attrib['status']
-                namedVersion=child.attrib['namedVersion']
+            
+                filePath=matchKeyAndGetVal(child, 'location') #child.attrib['location']
+                
+                temp=matchKeyAndGetVal(child, 'modTime')
+                
+                try:
+                    temp=float(temp)
+                except ValueError:
+                    print "Not a float"
+                    temp=None
+                    
+                if(temp):
+                    time = str(temp)+' = '+datetime.fromtimestamp( temp ).strftime(timeFormat)
+                else:
+                    time='N/A'
+                
+                status=matchKeyAndGetVal(child, 'status') #child.attrib['status']
+                namedVersion=matchKeyAndGetVal(child, 'namedVersion') #child.attrib['namedVersion']
+                
                 list.append( (filePath, time, status, namedVersion) )
         
         print list
@@ -156,7 +253,15 @@ def parseAndFillList(dataListStr):
     except Exception as e:
         print "Error. Couldn't finish parsing..."
         print e
+        traceback.print_exc() #
+        #sys.exc_info()
 
+def matchKeyAndGetVal(dict, keyPattern):
+    for key in dict.keys():
+        if(re.search(keyPattern, key, re.IGNORECASE)!=None):
+            return dict.attrib[key]
+    return 'N/A'
+        
 def getSelectedAndCall(commandStr):
     global resultListView
     print 'Appending Selected Items to the Command...'
@@ -164,13 +269,15 @@ def getSelectedAndCall(commandStr):
     for resultItem in resultListView.tree.selection():
         #resultItem is a coded identifier for the whole row
         temp = resultListView.tree.set(resultItem)
-        print temp[temp.keys()[0]]
-        appendStr=appendStr+' '+temp[temp.keys()[0]]
+        #print temp[temp.keys()[0]]
+        #appendStr=appendStr+' '+temp[temp.keys()[0]]
+        print temp['location']
+        appendStr=appendStr+' \"'+temp['location']+'\"'
     print appendStr
     subprocess.call(appendStr)
     print 'Executing Command...'
     
-###Ref. http://stackoverflow.com/questions/5286093/display-listbox-with-columns-using-tkinter 
+### Ref. http://stackoverflow.com/questions/5286093/display-listbox-with-columns-using-tkinter >>>
 """use a ttk.TreeView as a multicolumn ListBox"""
 class MultiColumnListbox(object):
 
@@ -183,11 +290,7 @@ class MultiColumnListbox(object):
     def _setup_widgets(self, titles=my_headers):
         global window
         
-        s = 'Click on header to sort by that column\nto change width of column, drag boundary'
-        msg = ttk.Label(window, wraplength="4i", justify="left", anchor="n",
-            padding=(10, 2, 10, 6), text=s)
-        msg.pack(fill='x')
-        container = ttk.Frame(window, width=1300, height=500) #ref. .grid_propagate(0) http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/ttk-Frame.html
+        container = ttk.Frame(window, width=1600, height=500) #ref. .grid_propagate(0) http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/ttk-Frame.html
         container.grid_propagate(0)
         container.pack(fill='both', expand=True)
         # create a treeview with dual scrollbars
@@ -207,6 +310,8 @@ class MultiColumnListbox(object):
     def _build_tree(self, titles=my_headers, items=my_list):
         self.tree.delete(*(self.tree.get_children()))
         
+        max_width=729
+        
         for col in titles:
             self.tree.heading(col, text=col.title(),
                 command=lambda c=col: sortby(self.tree, c, 0))
@@ -219,6 +324,8 @@ class MultiColumnListbox(object):
             # adjust column's width if necessary to fit each value
             for ix, val in enumerate(item):
                 col_w = tkFont.Font().measure(val)
+                if col_w>max_width: # limit width
+                    col_w=max_width
                 if self.tree.column(titles[ix],width=None)<col_w:
                     self.tree.column(titles[ix], width=col_w)
         
@@ -238,5 +345,7 @@ def sortby(tree, col, descending):
     # switch the heading so it will sort in the opposite direction
     tree.heading(col, command=lambda col=col: sortby(tree, col, \
         int(not descending)))
+
+### <<<
         
 main()
